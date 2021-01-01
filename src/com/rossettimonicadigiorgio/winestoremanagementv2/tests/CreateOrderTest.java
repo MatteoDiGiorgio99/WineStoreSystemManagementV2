eb0752706c7a594b7eb189e22c449a86657e8d36
Francesco Rossetti <16597707+francescorossetti@users.noreply.github.com>
2021-01-01 16:54:40 +0100

    01012021

--- src/com/rossettimonicadigiorgio/winestoremanagementv2/tests/CreateOrderTest.java
+++ src/com/rossettimonicadigiorgio/winestoremanagementv2/tests/CreateOrderTest.java
@@ -7,7 +7,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -19,54 +21,80 @@ import com.rossettimonicadigiorgio.winestoremanagementv2.classes.User;
 import com.rossettimonicadigiorgio.winestoremanagementv2.classes.Wine;
 import com.rossettimonicadigiorgio.winestoremanagementv2.frontend.Client;
 
+/**
+ * The {@code CreateOrderTest} is a class that defines the following behavior:
+ * Allows to test all the classes of the system that allows a user to create a order
+ * from the login.
+ * 
+ * @author 296666
+ *
+ */
+@TestMethodOrder(OrderAnnotation.class)
 public class CreateOrderTest {
 	private static ArrayList<Object> params = new ArrayList<Object>();
 	private static User user = null;
 	private static ArrayList<Wine> wines = new ArrayList<Wine>();
 		
+	/**
+	 * The method init
+	 * allows to reset the params arraylist
+	 * before every test
+	 */
 	@BeforeEach
-	static void init() {
-		params.clear();
+	public void init() {
+		CreateOrderTest.params.clear();
 	}
 	
-	@Test
-	static void testLogin() {
-		params.add("");
-		params.add("");
+	/**
+ 	 * The testlogin method
+	 * allows to simulate a login operation
+	 *  
+	 * @param email the user email to test
+	 * @param password the user password to test
+	 */
+	@ParameterizedTest
+	@CsvSource({"aa,aa"})
+	@org.junit.jupiter.api.Order(1)
+	public void testLogin(String email, String password) {
+		CreateOrderTest.params.add(email);
+		CreateOrderTest.params.add(password);
 		
-		Request request = new Request("userLogin",params);
+		Request request = new Request("userLogin", CreateOrderTest.params);
 		
 		Response response = new Client().run(request);
 		
-		user = (User) response.getValue();
+		CreateOrderTest.user = (User) response.getValue();
 		
-		assertNotNull(user);
+		assertNotNull(CreateOrderTest.user);
 	}
 	
-	@Test
-	static void testGetAllWines() {
-		wines = (ArrayList<Wine>) new Client().run(new Request("listWines", params)).getValue();
-		
-		assertNull(wines);
-		assertTrue(wines.size() >= 0);
-	}
-	
+	/**
+	 * The testResearchWine method
+	 * allows to simulate the research of a wine textbox-like
+	 * 
+	 * @param query the string of the name of the wine to search in the DB
+	 */
 	@ParameterizedTest
 	@CsvSource({ "vino1", "vino2" })
-	static void testReserchWines(String query) {
-		params.add(query);
+	@org.junit.jupiter.api.Order(2)
+	public void testReserchWines(String query) {
+		CreateOrderTest.params.add(query);
 		
-		wines = (ArrayList<Wine>) new Client().run(new Request("listWines", params)).getValue();
+		CreateOrderTest.wines = (ArrayList<Wine>) new Client().run(new Request("filterWines", params)).getValue();
 		
-		assertNull(wines);
-		assertTrue(wines.size() >= 0);
+		assertTrue(CreateOrderTest.wines.size() >= 0);
 	}
 	
+	/**
+	 * The method testInsertOrder
+	 * allows to submit a order into the database 
+	 */
 	@Test
-	static void testInsertOrder() {
+	@org.junit.jupiter.api.Order(3)
+	public void testInsertOrder() {
 		ArrayList<Wine> toOrder = new ArrayList<Wine>();
 		
-		for(Wine wine : wines) {
+		for(Wine wine : CreateOrderTest.wines) {
 			if(wine.getBottlesNumber() > 0) {
 				Wine toAdd = wine.clone();
 				toAdd.setBottlesNumber((int) (Math.random() * wine.getBottlesNumber()));
@@ -74,13 +102,12 @@ public class CreateOrderTest {
 			}
 		}
 		
-		Order order = new Order(-1, StatusEnum.Confirmed, user, toOrder);
+		Order order = new Order(-1, StatusEnum.Confirmed, CreateOrderTest.user, toOrder);
 		
-		params.add(order);
+		CreateOrderTest.params.add(order);
 		
-		Order result = (Order) new Client().run(new Request("insertOrder", params)).getValue();
+		Order result = (Order) new Client().run(new Request("insertOrder", CreateOrderTest.params)).getValue();
 		
-		assertNull(result);
 		assertTrue(result.getWines().size() == toOrder.size());
 	}
 }
